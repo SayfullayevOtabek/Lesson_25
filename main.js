1b4834ea8d19a4837aa0edb1ee98eb3abf9d4d39
SayfullayevOtabek <143277839+SayfullayevOtabek@users.noreply.github.com>
2023-10-05 23:08:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+
+// Begin1.
+// const  a = parseInt(prompt("a ga qiymat kiriting"));
+// const S = 4 * a;
+// alert(S);
+
+
+// Begin2.
+// const  a = parseInt(prompt("a  qiymat kiriting"));
+// const S = a * a;
+// alert(S);
+
+// Begin3.
+// const  a = parseInt(prompt("a ga qiymat kiriting"));
+// const  b = parseInt(prompt("b ga qiymat kiriting"));
+// const S = a * b; 
+// const P = 2 * (a + b);
+// alert(S);
+// alert(P);
+
+
+// Begin4.
+// const  d = parseInt(prompt("d ga qiymat kiriting"));
+// const L = 3.14 * d;
+// alert(L);
+
+
+// Begin5.
+// const  a = parseInt(prompt("a ga qiymat kiriting"));
+// const V = a * a * a; 
+// const S = 6 * a * a;
+// alert(V);
+// alert(S);
+
+
+// Begin6.
+// const  a = parseInt(prompt("a ga qiymat kiriting"));
+// const  b = parseInt(prompt("b ga qiymat kiriting"));
+// const  c = parseInt(prompt("c ga qiymat kiriting"));
+// const V = a * b * c; 
+// const S = 2 * (a * b + b * c + a * c);
+// alert(V);
+// alert(S);
+
+
+
+// Begin7.
+// const  R = parseInt(prompt("R ga qiymat kiriting"));
+// const L = 2 * 3.14 * R; 
+// const S = 3.14 * R * R;
+// alert(L);
+// alert(S);
+
+
+
+// Begin8.
+// const  a = parseInt(prompt("a ga qiymat kiriting"));
+// const  b = parseInt(prompt("b ga qiymat kiriting"));
+// const S = (a + b)/2;
+// alert(S);
